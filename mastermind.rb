aa8bc471dab514a411ef79a429d85e3f7abea331
seansg <seansgarwood@gmail.com>
2023-08-16 22:05:21 -0400

    Basic classes

---
+++ mastermind.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal = true
+
+# instantiate a game
+class Game
+
+end
+
+# actual code, i.e. the win condition
+class WinCondition
+
+end
+
+# instantiate guesses
+class Guess
+
+end
