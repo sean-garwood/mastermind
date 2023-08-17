72c5158175c6d1338fb4192fe1b3cd55843bc3f1
seansg <seansgarwood@gmail.com>
2023-08-16 22:29:51 -0400

    Define guess methods

--- mastermind.rb
+++ mastermind.rb
@@ -1,16 +1,52 @@
 # frozen_string_literal = true
 
+module Answer
+  COLORS = {
+    red: 0,
+    orange: 1,
+    yellow: 2,
+    green: 3,
+    blue: 4,
+    violet: 5,
+  }
+
+  POSITIONS = [1, 2, 3, 4]
+
+  def rng_answer
+    color_index = (0..5).rand
+    position_index = (0..3).rand
+    [color_index, position_index]
+  end
+end
+
 # instantiate a game
 class Game
 
-end
+  include Answer
+  @@turns_left = 12
 
-# actual code, i.e. the win condition
-class WinCondition
+  def initialize()
 
+  end
+
+  def take_turn
+
+  end
+
+  def over?
+
+  end
 end
 
 # instantiate guesses
 class Guess
+  def correct?
+    guess == code
+  end
+end
 
+class Player
+  def initialize()
+
+  end
 end
