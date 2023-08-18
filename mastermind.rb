4bdf17c268a2c4a12a10545af8f48012c1a3fac3
seansg <seansgarwood@gmail.com>
2023-08-18 14:20:01 -0400

    Define #guess

--- mastermind.rb
+++ mastermind.rb
@@ -1,32 +1,23 @@
-# frozen_string_literal = true
+# frozen_string_literal = TRUE
 
+# random guess generator
 module Answer
-  COLORS = {
-    red: 0,
-    orange: 1,
-    yellow: 2,
-    green: 3,
-    blue: 4,
-    violet: 5,
-  }
+  COLORS = %w[red orange yellow green blue violet].freeze
+  POSITIONS = (0..3).freeze
 
-  POSITIONS = [1, 2, 3, 4]
-
-  def rng_answer
-    color_index = (0..5).rand
-    position_index = (0..3).rand
-    [color_index, position_index]
+  def guess
+    guess = {}
+    POSITIONS.each { |position| guess[position] = COLORS.sample }
+    guess
   end
 end
 
 # instantiate a game
 class Game
-
   include Answer
-  @@turns_left = 12
 
   def initialize()
-
+    @turns = 12
   end
 
   def take_turn
