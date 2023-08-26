18caa84c6ac756a3858bdca50a46652700e18a99
seansg <seansgarwood@gmail.com>
2023-08-26 01:37:38 -0400

    Add newlines after headers

--- talk.rb
+++ talk.rb
@@ -4,21 +4,23 @@ module Talk
     puts "---------Welcome to Mastermind!---------\n"
     describe_board
     describe_object_of_game
-    provide_legend
+    show_legends
     choose_role
   end
 
   def choose_role
     role = <<~ROLE
-      ---------Role---------
-      Choose your role. Will you be a codemaker or a heartbreaker?
+      ---------Role---------\n
+      Choose your role. Will you be a codemaker or a heartbreaker?\n
+      (press Enter to be the breaker, anything else to watch the computer
+      wreck your code.)
     ROLE
     puts role
   end
 
   def describe_board
     description = <<~DESC
-      ----------The board----------
+      ----------The board----------\n
       The board is twelve rows by eight columns: four columns each for the
       previous guesses on the left, and four columns containing feedback
       for those guesses.
@@ -28,7 +30,7 @@ module Talk
 
   def describe_object_of_game
     object_of_game = <<~OBJ
-      ---------Object of game---------
+      ---------Object of game---------\n
       You have twelve turns to guess the correct code, which may contain
       duplicate values. You will be provided feedback at the end of each
       turn. See the legend for details
@@ -36,23 +38,28 @@ module Talk
     puts object_of_game
   end
 
-  def color_legend
+  def show_color_legend
     color_legend = <<~LEGEND
-      ---------Legend---------
-      ---------Color pegs---------
+      ---------Legend---------\n
+      ---------Color pegs---------\n
       r: Red, o: Orange, y: Yellow, g: Green, b: Blue, v: Violet
     LEGEND
     puts color_legend
   end
 
-  def guess_legend
+  def show_guess_legend
     guess_legend = <<~LEGEND
-      ---------Guess pegs---------
+      ---------Guess pegs---------\n
       x - incorrect color and position
       o - correct color, incorrect position
       c - correct color and position
     LEGEND
     puts guess_legend
+  end
+
+  def show_legends
+    show_color_legend
+    show_guess_legend
   end
 
   def reminder
@@ -74,4 +81,5 @@ module Talk
     end
     puts 'Computers always win.'
   end
+
 end
