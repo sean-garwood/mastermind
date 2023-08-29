546e932971d7b069ab30923698fcbe3bc3c0c75c
seansg <seansgarwood@gmail.com>
2023-08-29 14:30:51 -0400

    Fix #first_guess

--- code_helper.rb
+++ code_helper.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 # transform user input to translate code
 module CodeHelper
@@ -14,20 +15,18 @@ module CodeHelper
     Array.new(members) { element }
   end
 
-  def set_code_and_guess
-    if maker?
-      @code = pick_random_colors
-      @guess = nil
-    else
-      @code = take_input
-      @guess = first_guess
-    end
+  def first_guess
+    first_color = random_color
+    color_pool = COLORS.reject { |color| color == first_color }
+    second_color = color_pool.sample
+    first_guess = []
+    2.times { first_guess << first_color }
+    2.times { first_guess << second_color }
+    first_guess
   end
 
-  def first_guess
-    pair = to_a(2, COLORS.sample)
-    two_pair = to_a(2, pair)
-    two_pair.flatten.sort
+  def random_color
+    COLORS.sample
   end
 
   def pick_random_colors
