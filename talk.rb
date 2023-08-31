045036413ab2778d9e51e7b558b33e2672572571
seansg <seansgarwood@gmail.com>
2023-08-30 23:01:29 -0400

    Remove useless methods

--- talk.rb
+++ talk.rb
@@ -71,20 +71,4 @@ module Talk
   def prompt_user_for_guess
     puts "\nEnter your guess as a string of four letters. Order matters!\n\n"
   end
-
-  def bad_end
-    puts '----Oh no!----'
-  end
-
-  def good_end
-    puts '----Yay!----'
-  end
-
-  def display_computer_results(guesses)
-    puts 'Computer guesses:'
-    guesses.each_with_index do |guess, i|
-      puts "#{i + 1}. #{guess}"
-    end
-    puts 'Computers always win.'
-  end
 end
